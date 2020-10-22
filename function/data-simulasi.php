abe581301d76e36d28d899bd106b7f627bb2cc37
Ravino Rahman <ravinorahman@gmail.com>
2020-10-22 14:59:18 +0700

    del data-simulasi

--- function/data-simulasi.php
+++
@@ -1,9 +0,0 @@
-<?php include'function.php';
-
-$id_barang = $_POST["id"];
-$data_harga = tampil_data("SELECT HARGA_JUAL FROM master_barang WHERE ID_BARANG = '$id_barang'");
-foreach ($data_harga as $harga) {
-  echo $harga["HARGA_JUAL"];
-}
-
-?>
